2d318f086ac6cb07fad4e2be756298e4d625d5c1
Aidar Yessembayev <yessembayevaidar@gmail.com>
2023-02-12 18:52:00 +0600

    init 4

--- utils/src/main/java/com/epam/utils/StringUtils.java
+++ utils/src/main/java/com/epam/utils/StringUtils.java
@@ -1,32 +1,10 @@
 package com.epam.utils;
 
+import org.apache.commons.lang3.math.NumberUtils;
+
 public class StringUtils {
     public static boolean isPositiveNumber(String str) {
          //here magic will happen
-        if (str.isEmpty()) {
-            return false;
-        }
-
-        try {
-            int number = Integer.parseInt(str);
-
-            if (number > 0) {
-                return true;
-            }
-
-        } catch(NumberFormatException e) {
-            float number_2 = Float.parseFloat(str);
-
-            if (number_2 > 0.0) {
-                return true;
-            }
-
-            return false;
-
-        } catch(NullPointerException e) {
-            return false;
-        }
-
-        return false;
+        return NumberUtils.isCreatable(str) && NumberUtils.toDouble(str) > 0;
     }
 }
