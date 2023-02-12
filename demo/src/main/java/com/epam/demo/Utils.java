ec2241bedbf7d6904f32f191cd60e903b4f0eecc
Aidar Yessembayev <yessembayevaidar@gmail.com>
2023-02-12 18:28:42 +0600

    init

--- demo/src/main/java/com/epam/demo/Utils.java
+++ demo/src/main/java/com/epam/demo/Utils.java
@@ -1,9 +1,18 @@
 package com.epam.demo;
 
+import com.epam.utils.StringUtils;
+
 import java.util.List;
 
 public class Utils {
     public static boolean isAllPositiveNumbers(List<String> args) {
         //magic happens here
+        for (int i = 0; i < args.size(); i++) {
+            if (!StringUtils.isPositiveNumber(args.get(i))) {
+                return false;
+            }
+        }
+
+        return true;
     }
 }
--- utils/src/main/java/com/epam/utils/StringUtils.java
+++ utils/src/main/java/com/epam/utils/StringUtils.java
@@ -3,5 +3,12 @@ package com.epam.utils;
 public class StringUtils {
     public static boolean isPositiveNumber(String str) {
          //here magic will happen
+        int number = Integer.parseInt(str);
+
+        if (number > 0) {
+            return true;
+        }
+
+        return false;
     }
 }
